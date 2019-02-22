a288fb2f683f93626f216ebfd22df1329de989cd
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-02-22 09:12:44 -0300

    MAIN.JS mostrando pokemones en la pagina principal, INDEX.HTML agregando class CONTAINER-FLUID y STYLE.CSS agregando estilo a las tarjetas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
-const pokeData = (POKEMON);
-const newData = (pokeData)=>{
-    let result = '';
-   // console.log(pokeData)
-}
+const pokeData = POKEMON.pokemon;
+const container = document.getElementById('root');// donde contendre mis cartas con pokemon
+
+const newData = (pokeData) => {
+	let result = '';
+	//console.log(DATA)
+   pokeData.forEach(element => {
+	 // element --> DATA[i]
+	 //console.log(element.house);
+	 
+  
+		 result = container.innerHTML += `
+		 <div>
+			 <div class="card">
+             <div class="box">
+             <h2>${element.name}</h2>
+				 <div class="img">
+                     <img src="${element.img}">
+                     <p>Tipo:${element.type}</p>
+				 </div>				 
+			 </div>
+			 </div>
+		 </div>` 
+	 } 
+   );
+   return result;
+ }
+
+ window.onload = newData(pokeData);
