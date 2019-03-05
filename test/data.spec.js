f8f06e0353832bdf04009b3598e18776e1583643
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-03-05 10:14:06 -0300

    DATA.SPEC.JS subiendo tests de la función filtrar

--- test/data.spec.js
+++ test/data.spec.js
@@ -6,14 +6,80 @@ require('./data.spec.js');
 // const data = window.POKEMON.pokemon;
 
 describe('filterData', () => {
+  const waterData = [
+    {
+      name: 'Squirtle',
+      type: 'Water'
+    },
+    {
+      name: 'Wartortle',
+      type: 'Water'
+    },
+    {
+      name: 'Blastoise',
+      type: 'Water'
+    },
+    {
+      name: 'Psyduck',
+      type: 'Water'
+    }
+
+  ]
+
+  const psychicData = [
+    {
+      name: 'Mr. Mime',
+      type: 'Psychic'
+    },
+    {
+      name: 'Jynx',
+      type: 'Psychic'
+    },
+    {
+    name: 'Mewtwo',
+    type: 'Psychic'
+  },
+  {
+    name: 'Mew',
+    type: 'Psychic'
+  }
+  ]
+
+  const fireData = [
+    {
+      name: 'Charmander',
+      type: 'Fire'
+    },
+    {
+      name: 'Charizard',
+      type: 'Fire'
+    },
+    {
+    name: 'Vulpix',
+    type: 'Fire'
+  },
+  {
+    name: 'Ninetales',
+    type: 'Fire'
+  }
+  ]
   
-  it('debería ser una función', () => {
+  it('Debería ser una función', () => {
     assert.equal(typeof filterData, 'function');
   });
 
-  // it('Debería retornar Squirtle como primer pokémon de tipo agua', () => {
-  //   assert.equal(filterData(data,'Water')[0].name, 'Squirtle');
-  // });
+  it('Debería retornar Squirtle como primer pokémon de tipo Agua', () => {
+    assert.equal(window.filterData(waterData,'Water')[0].name, 'Squirtle');
+  });
+
+  it('Debería retornar Mew como ultimo pokémon de tipo Psychic', () => {
+    assert.equal(window.filterData(psychicData,'Psychic')[3].name, 'Mew');
+  });
+
+  it('Debería retornar Charizard como segundo pokémon de tipo Fire', () => {
+    assert.equal(window.filterData(fireData,'Fire')[1].name, 'Charizard');
+  });
+
   describe('sortData', () => {
     
     it('Debería ser una función', () => {
