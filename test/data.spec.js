2a5ff0726094f7c30e9e91530b303b0f15ebe344
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-03-11 10:13:08 -0300

    DATA.SPEC.JS corrigiendo test

--- test/data.spec.js
+++ test/data.spec.js
@@ -77,14 +77,72 @@ describe('filterData', () => {
 
   it('Debería retornar Charizard como segundo pokémon de tipo Fire', () => {
     assert.equal(window.filterData(data,'Fire')[1].name, 'Charizard');
-  });
+  })
+});
 
   describe('sortData', () => {
+    const data = [
+      {
+        name: 'Squirtle',
+        type: 'Water'
+      },
+      {
+        name: 'Wartortle',
+        type: 'Water'
+      },
+      {
+        name: 'Blastoise',
+        type: 'Water'
+      },
+      {
+        name: 'Psyduck',
+        type: 'Water'
+      },
+      {
+        name: 'Abra',
+        type: 'Psychic'
+      },
+      {
+        name: 'Mr. Mime',
+        type: 'Psychic'
+      },
+      {
+        name: 'Jynx',
+        type: 'Psychic'
+      },
+      {
+      name: 'Mewtwo',
+      type: 'Psychic'
+    },
+    {
+      name: 'Mew',
+      type: 'Psychic'
+    },
+      {
+        name: 'Charmander',
+        type: 'Fire'
+      },
+      {
+        name: 'Charizard',
+        type: 'Fire'
+      },
+      {
+      name: 'Vulpix',
+      type: 'Fire'
+    },
+    {
+      name: 'Ninetales',
+      type: 'Fire'
+    },
+    {
+      name: 'Zubat',
+      type: 'Grass'
+    }
+    ]
     
     it('Debería ser una función', () => {
       assert.equal(typeof sortData, 'function');
-    });
-  })
+    })
 
   it('Debería retornar Abra como primer pokémon de la A-Z', () => {
     assert.equal(window.sortData(data,'name','a--z')[0].name, 'Abra');
@@ -101,6 +159,7 @@ describe('filterData', () => {
   it('Debería retornar Abra como ultimo pokémon de la Z-A', () => {
     assert.equal(window.sortData(data,'name','z--a')[13].name, 'Abra');
   })
-
 });
+
+
 
