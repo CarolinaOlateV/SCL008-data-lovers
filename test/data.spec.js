604cac89fc1a19369f53247a53fa45cbadd49a38
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-03-14 18:01:02 -0300

    DATA.SPEC.JS agregando test de la función computeStats y agregando test de la función numData

--- test/data.spec.js
+++ test/data.spec.js
@@ -162,16 +162,162 @@ describe('filterData', () => {
 });
 
 describe('computeStats',() => {
+  
+  const data = [
+    {
+      name: 'Squirtle',
+      type: 'Water'
+    },
+    {
+      name: 'Wartortle',
+      type: 'Water'
+    },
+    {
+      name: 'Blastoise',
+      type: 'Water'
+    },
+    {
+      name: 'Psyduck',
+      type: 'Water'
+    },
+    {
+      name: 'Abra',
+      type: 'Psychic'
+    },
+    {
+      name: 'Mr. Mime',
+      type: 'Psychic'
+    },
+    {
+      name: 'Jynx',
+      type: 'Psychic'
+    },
+    {
+    name: 'Mewtwo',
+    type: 'Psychic'
+  },
+  {
+    name: 'Mew',
+    type: 'Psychic'
+  },
+    {
+      name: 'Charmander',
+      type: 'Fire'
+    },
+    {
+      name: 'Charizard',
+      type: 'Fire'
+    },
+    {
+    name: 'Vulpix',
+    type: 'Fire'
+  },
+  {
+    name: 'Ninetales',
+    type: 'Fire'
+  },
+  {
+    name: 'Zubat',
+    type: 'Grass'
+  }
+  ]
 
   it('Debería ser una función', () => {
     assert.equal(typeof computeStats, 'function');
   })
+
+  it('Debería retornar 4 de pokémones tipo agua', () => {
+    assert.equal(window.computeStats(data, 'Water'), "4");
+  })
 });
 
 describe('numData', () => {
+
+  const data = [
+    {
+      name: 'Squirtle',
+      type: 'Water',
+      num: '01'
+    },
+    {
+      name: 'Wartortle',
+      type: 'Water',
+      num: '02'
+    },
+    {
+      name: 'Blastoise',
+      type: 'Water',
+      num: '03'
+    },
+    {
+      name: 'Psyduck',
+      type: 'Water',
+      num: '04'
+    },
+    {
+      name: 'Abra',
+      type: 'Psychic',
+      num: '05'
+    },
+    {
+      name: 'Mr. Mime',
+      type: 'Psychic',
+      num: '06'
+    },
+    {
+      name: 'Jynx',
+      type: 'Psychic',
+      num: '07'
+    },
+    {
+    name: 'Mewtwo',
+    type: 'Psychic',
+    num: '08'
+  },
+  {
+    name: 'Mew',
+    type: 'Psychic',
+    num: '09'
+  },
+    {
+      name: 'Charmander',
+      type: 'Fire',
+      num: '10'
+    },
+    {
+      name: 'Charizard',
+      type: 'Fire',
+      num: '11'
+    },
+    {
+    name: 'Vulpix',
+    type: 'Fire',
+    num: '12'
+  },
+  {
+    name: 'Ninetales',
+    type: 'Fire', 
+    num: '13'
+  },
+  {
+    name: 'Zubat',
+    type: 'Grass',     
+    num: '14'
+  }
+  ]
+
   it('Debería ser una función', () => {
     assert.equal(typeof numData, 'function');
 })
+
+it('Debería retornar Squirtle como primer pokémon del 01 al 14', () => {
+  assert.equal(window.numData(data,'num')[0].name, 'Squirtle');
+})
+
+it('Debería retornar Zubat como primer pokémon del 14 al 01', () => {
+  assert.equal(window.numData(data,'name','num-last')[0].name, 'Zubat');
+})
+
 });
 
 
